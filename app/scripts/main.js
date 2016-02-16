df2235cfc4531a18cc5d156a2e9341d024453ca7
Mattia Asti <mattia.asti@gmail.com>
2016-02-16 17:50:59 +0000

    work in progress

---
+++ app/scripts/main.js
@@ -0,0 +1,13 @@
+(function() {
+
+  // same height membership columns
+  var membershipColumns = document.querySelectorAll('.same-height .message-body');
+  var membershipColumnsMaxHeight = 0;
+  Array.prototype.forEach.call(membershipColumns, function(el) {
+    membershipColumnsMaxHeight = Math.max(membershipColumnsMaxHeight, el.offsetHeight);
+  });
+  Array.prototype.forEach.call(membershipColumns, function(el) {
+    el.style.minHeight = membershipColumnsMaxHeight + 'px';
+  });
+
+})();

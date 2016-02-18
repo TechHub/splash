9d9b55b52b37eb667f58552ab787d66dcd108cc1
Mattia Asti <mattia.asti@gmail.com>
2016-02-18 18:06:33 +0000

    small changes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -59,7 +59,13 @@ const testLintOptions = {
   }
 };
 
-gulp.task('lint', lint('app/scripts/**/*.js'));
+const lintOptions = {
+  globals: {
+    'jQuery': true
+  }
+}
+
+gulp.task('lint', lint('app/scripts/**/*.js', lintOptions));
 gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
 
 gulp.task('html', ['styles', 'scripts'], () => {
